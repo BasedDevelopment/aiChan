3b1760e4675dc87714f9e9b4c8bd2280c63d1c3c
Ezri Zhu <me@ezrizhu.com>
2024-03-02 01:30:53 -0500

    merge main

--- newMsg.go
+++ newMsg.go
@@ -1,5 +1,5 @@
 /*
-   Copyright (C) 2022 Tianyu Zhu eric@ericz.me
+   Copyright (C) 2022-2024 Ezri Zhu me@ezrizhu.com
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
